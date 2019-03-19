b52d3ae6dd86161b81f706991c59b71114b89cf2
Miguel Ribeiro Neto <miguelribeironeto@MacBook-Pro-de-Miguel-2.local>
2019-03-19 14:49:09 +0100

    created counter component

---
+++ src/Counter.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { connect } from "react-redux"
+
+class Counter extends Component {
+
+  increment = () => {
+
+
+  }
+
+  decrement = () => {
+
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Counter</h2>
+        <div>
+          <button onClick={this.decrement}>-</button>
+          <span>{this.props.count}</span>
+          <button onClick={this.increment}>+</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default connect()(Counter);
